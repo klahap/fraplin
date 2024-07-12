d5d8e8f3e121954cd1b14846e8cf912dfd8f707a
Klaus Happacher <k.happacher@gmail.com>
2024-07-12 09:42:15 +0200

    better error messages

--- src/main/resources/default_code/util/HttpUtil.kt
+++ src/main/resources/default_code/util/HttpUtil.kt
@@ -9,14 +9,19 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 
 sealed class HttpException(msg: String) : Exception(msg) {
-    sealed class ClientError(msg: String): HttpException(msg) {
-        class NotFound : ClientError("404, not found")
-        class Conflict : ClientError("409, conflict")
-        class Other(msg: String): ClientError(msg)
+    sealed class ClientError(msg: String) : HttpException(msg) {
+        class NotFound(msg: String? = null) : ClientError("404, not found${msg.appendMsg}")
+        class Conflict(msg: String? = null) : ClientError("409, conflict${msg.appendMsg}")
+        class Other(msg: String) : ClientError(msg)
     }
-    sealed class ServerError(msg: String): HttpException(msg) {
-        class InternalServerError: ServerError("500, Internal Server Error")
-        class Other(msg: String): ServerError(msg)
+
+    sealed class ServerError(msg: String) : HttpException(msg) {
+        class InternalServerError(msg: String? = null) : ServerError("500, Internal Server Error${msg.appendMsg}")
+        class Other(msg: String) : ServerError(msg)
+    }
+
+    companion object {
+        private val String?.appendMsg get() = this?.let { ", $it" } ?: ""
     }
 }
 
@@ -30,10 +35,10 @@ inline fun <reified T> Response.getJsonIfSuccessfulOrThrow(json: Json = Json): T
 fun Response.throwIfError() {
     if (isSuccessful) return
     val msg = message.takeIf { it.isNotBlank() } ?: body?.string()
-    when(code) {
-        404 -> throw HttpException.ClientError.NotFound()
-        409 -> throw HttpException.ClientError.Conflict()
-        500 -> throw HttpException.ServerError.InternalServerError()
+    when (code) {
+        404 -> throw HttpException.ClientError.NotFound(msg)
+        409 -> throw HttpException.ClientError.Conflict(msg)
+        500 -> throw HttpException.ServerError.InternalServerError(msg)
         in 400..499 -> throw HttpException.ClientError.Other("$code, $msg")
         in 500..599 -> throw HttpException.ServerError.Other("$code, $msg")
     }
